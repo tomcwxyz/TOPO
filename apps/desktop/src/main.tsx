b17a25bb2899845b7fe84a78b442a2f61a86e867
Tom C Watson <150357824+tomcwxyz@users.noreply.github.com>
2026-09-06 22:38:39 +0100

    Route first run through guided setup

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { SetupGate } from "./SetupGate";
 import "./styles.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <SetupGate>
+      <App />
+    </SetupGate>
+  </React.StrictMode>,
 );
