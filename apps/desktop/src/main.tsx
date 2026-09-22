de505796edee99d01f0b2bce28351438c4fc9080
Tom C Watson <150357824+tomcwxyz@users.noreply.github.com>
2026-09-22 22:12:50 +0100

    feat(alpha4): mount terrain brand on calm shell

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { CalmApp } from "./CalmApp";
 import { SetupGate } from "./SetupGate";
+import { TopoTerrainBridge } from "./TopoVisuals";
 import "./styles.css";
 import "./memory-pages.css";
+import "./topo-brand.css";
+import "./topo-primitives.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SetupGate>
-      <CalmApp />
-    </SetupGate>
+    <>
+      <SetupGate>
+        <CalmApp />
+      </SetupGate>
+      <TopoTerrainBridge />
+    </>
   </React.StrictMode>,
 );
